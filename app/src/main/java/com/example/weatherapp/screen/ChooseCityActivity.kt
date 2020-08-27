31e9d1459bd30a7b0283bb3a2eea720c709275e7
Mishabuzov <mikhaijl7@gmail.com>
2020-08-27 11:21:55 +0300

    replaced data obtaining to ViewModel class

--- app/src/main/java/com/example/weatherapp/screen/ChooseCityActivity.kt
+++ app/src/main/java/com/example/weatherapp/screen/ChooseCityActivity.kt
@@ -1,54 +1,40 @@
 package com.example.weatherapp.screen
 
 import android.os.Bundle
-import android.util.Log.e
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.weatherapp.Config
 import com.example.weatherapp.R
-import com.example.weatherapp.repository.WeatherProvider
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
+import com.example.weatherapp.content.DailyWeather
 import kotlinx.android.synthetic.main.activity_choose_city.*
 
-class ChooseCityActivity : AppCompatActivity() {
+class ChooseCityActivity : AppCompatActivity(), CityViewModel.CitiesCallback {
 
     private val weatherViewModel by lazy {
-        ViewModelProvider(this)
-            .get(CityViewModel::class.java)
+        ViewModelProvider(this, CityViewModel.CityViewModelFactory(this))
+            .get(CityViewModel(this)::class.java)
     }
+
+    private val adapter = ChooseCityAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_city)
 
-        val adapter = ChooseCityAdapter()
         cityList.layoutManager = LinearLayoutManager(this)
         cityList.adapter = adapter
-        weatherViewModel.getCityWeather().observe(this, Observer {
-            it?.let {
-                adapter.refreshWeather(it)
-            }
+        val currentLiveData = weatherViewModel.getCityWeather()
+        currentLiveData.observe(this, Observer {
+            it?.let { refreshAdapter(it) }
         })
-        processWeatherRequest(adapter)
+        if (currentLiveData.value.isNullOrEmpty()) {
+            weatherViewModel.processWeatherRequest()
+        }
     }
 
-    fun processWeatherRequest(adapter: ChooseCityAdapter) {
-        WeatherProvider.weatherRepository
-            .getCurrentWeather(Config.citiesIds.values.joinToString(separator = ","))
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe(
-                {
-                    weatherViewModel.updateCityWeather(it.dailyWeather)
-                    adapter.refreshWeather(it.dailyWeather)
-                },
-                {
-                    e("REQUEST ERROR", "Error getting city weather list")
-                }
-            )
+    override fun refreshAdapter(cities: List<DailyWeather>) {
+        adapter.refreshWeather(cities)
     }
 
 }
--- app/src/main/java/com/example/weatherapp/screen/CityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/screen/CityViewModel.kt
@@ -1,17 +1,48 @@
 package com.example.weatherapp.screen
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.Config
 import com.example.weatherapp.content.DailyWeather
+import com.example.weatherapp.repository.WeatherProvider
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class CityViewModel : ViewModel() {
+class CityViewModel(private val citiesCallback: CitiesCallback) : ViewModel() {
 
-    val cityWeatherList: MutableLiveData<List<DailyWeather>> = MutableLiveData()
+    private val cityWeatherList: MutableLiveData<List<DailyWeather>> = MutableLiveData()
 
     fun getCityWeather(): MutableLiveData<List<DailyWeather>> = cityWeatherList
 
-    fun updateCityWeather(cityWeatherList: List<DailyWeather>) {
-        this.cityWeatherList.value = cityWeatherList
+    fun processWeatherRequest() {
+        WeatherProvider.weatherRepository
+            .getCurrentWeather(Config.citiesIds.values.joinToString(separator = ","))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                {
+                    cityWeatherList.value = it.dailyWeather
+                    citiesCallback.refreshAdapter(it.dailyWeather)
+                },
+                {
+                    Log.d(
+                        "M_CityViewModel",
+                        "Error getting city weather list\n${it.stackTrace}"
+                    )
+                }
+            )
+    }
+
+    class CityViewModelFactory(private val citiesCallback: CitiesCallback) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return modelClass.getConstructor(CitiesCallback::class.java).newInstance(citiesCallback)
+        }
+    }
+
+    interface CitiesCallback {
+        fun refreshAdapter(cities: List<DailyWeather>)
     }
 
 }
