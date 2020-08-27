f45b2ea2aba56444d3b5565208160df3d79ec17a
Mishabuzov <mikhaijl7@gmail.com>
2020-08-27 11:18:50 +0300

    cleaned code, added logging interceptor

--- app/src/main/java/com/example/weatherapp/api/ApiFactory.kt
+++ app/src/main/java/com/example/weatherapp/api/ApiFactory.kt
@@ -4,6 +4,7 @@ import com.example.weatherapp.Config
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,19 +14,25 @@ object ApiFactory {
         buildRetrofit().create(WeatherService::class.java)
     }
 
-    private fun buildRetrofit(): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(Config.WEATHER_ENDPOINT_DEBUG)
-            .client(buildClient())
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create()) //rx wrapper
-            .build()
-
-    private fun buildClient(): OkHttpClient {
-        val httpClient = OkHttpClient.Builder()
-            .addInterceptor(ApiKeyInterceptor.create())
-        httpClient.networkInterceptors().add(StethoInterceptor())
-        return httpClient.build()
+    // HttpLoggingInterceptor выводит подробности сетевого запроса в логи
+    private val loggingInterceptor: HttpLoggingInterceptor by lazy {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
     }
+
+    private fun buildRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(Config.WEATHER_ENDPOINT_DEBUG)
+        .client(buildClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create()) //rx wrapper
+        .build()
+
+    private fun buildClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(ApiKeyInterceptor.create())
+        .addInterceptor(loggingInterceptor)
+        .addNetworkInterceptor(StethoInterceptor())
+        .build()
 
 }
