121ff652eb28eec5f292cd200a4548ce9530d66c
Mishabuzov <mikhaijl7@gmail.com>
2020-08-27 18:49:35 +0300

    added decoration with cosmetic effect for RecyclerView

---
+++ app/src/main/java/com/example/weatherapp/screen/DividerItemDecoration.kt
@@ -0,0 +1,97 @@
+package com.example.weatherapp.screen
+
+import android.R
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class DividerItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+
+    private val mDivider: Drawable? by lazy {
+        val a = context
+            .obtainStyledAttributes(intArrayOf(R.attr.listDivider))
+        val drawable = a.getDrawable(0)
+        a.recycle()
+        drawable
+    }
+
+    private var mOrientation = -1
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        if (mDivider == null) {
+            return
+        }
+        val position = parent.getChildAdapterPosition(view)
+        if (position == RecyclerView.NO_POSITION || position == 0) {
+            return
+        }
+        if (mOrientation == -1) {
+            getOrientation(parent)
+        }
+        if (mOrientation == LinearLayoutManager.VERTICAL) {
+            outRect.top = mDivider?.intrinsicHeight ?: 0
+        } else {
+            outRect.left = mDivider?.intrinsicWidth ?: 0
+        }
+    }
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        if (mDivider == null) {
+            super.onDrawOver(c, parent, state)
+            return
+        }
+
+        // Initialization needed to avoid compiler warning
+        var left = 0
+        var right = 0
+        var top = 0
+        var bottom = 0
+        val size: Int
+        val orientation = if (mOrientation != -1) mOrientation else getOrientation(parent)
+        val childCount = parent.childCount
+        if (orientation == LinearLayoutManager.VERTICAL) {
+            size = mDivider?.intrinsicHeight ?: 0
+            left = parent.paddingLeft
+            right = parent.width - parent.paddingRight
+        } else { //horizontal
+            size = mDivider?.intrinsicWidth ?: 0
+            top = parent.paddingTop
+            bottom = parent.height - parent.paddingBottom
+        }
+        for (i in 1 until childCount) {
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+            if (orientation == LinearLayoutManager.VERTICAL) {
+                top = child.top - params.topMargin - size
+                bottom = top + size
+            } else { //horizontal
+                left = child.left - params.leftMargin
+                right = left + size
+            }
+            mDivider?.setBounds(left, top, right, bottom)
+            mDivider?.draw(c)
+        }
+        // show last divider
+    }
+
+    private fun getOrientation(parent: RecyclerView): Int {
+        if (mOrientation == -1) {
+            if (parent.layoutManager is LinearLayoutManager) {
+                val layoutManager: LinearLayoutManager =
+                    parent.layoutManager as LinearLayoutManager
+                mOrientation = layoutManager.orientation
+            }
+        }
+        return mOrientation
+    }
+}
