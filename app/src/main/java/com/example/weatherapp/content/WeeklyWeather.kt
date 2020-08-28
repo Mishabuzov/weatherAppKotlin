2660361262582864cad00511f11e5abba292f70c
Mishabuzov <mikhaijl7@gmail.com>
2020-08-28 17:31:23 +0300

    implemented WeeklyWeather model

--- app/src/main/java/com/example/weatherapp/content/WeeklyWeather.kt
+++ app/src/main/java/com/example/weatherapp/content/WeeklyWeather.kt
@@ -1,31 +1,35 @@
 package com.example.weatherapp.content
 
+import androidx.annotation.DrawableRes
 import com.google.gson.annotations.SerializedName
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 data class WeeklyWeatherWrapper(
-    @SerializedName("list") val weeklyWeatherList: List<WeatherData>,
+    @SerializedName("list") val weeklyWeeklyWeatherNetworkList: List<WeeklyWeatherNetworkData>,
     val city: City
 )
 
 data class City(val name: String)
 
-data class WeatherData(
+data class WeeklyWeatherNetworkData(
     @SerializedName("dt") val date: Long,
     @SerializedName("temp") val temperature: Temperature,
     @SerializedName("weather") val weatherDescriptionList: List<WeatherDescription>,
     val pressure: Double,
     val humidity: Int,
     val speed: Double
-) {
-    fun getHumanReadableDate(): String {
-        val date = Date(date * 1000)
-        val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-        sdf.timeZone = TimeZone.getTimeZone("GMT-4")
-        return sdf.format(date)
-    }
-}
+)
 
 data class Temperature(val day: Double, val min: Double, val max: Double)
+
+data class WeeklyWeather(
+    val temperature: Int,
+    val weatherDescription: String,
+    @DrawableRes val weatherIconResource: Int,
+    val humanReadableDate: String,
+    val pressure: Int,
+    val humidity: Int,
+    val windSpeed: Double
+)
+
+
