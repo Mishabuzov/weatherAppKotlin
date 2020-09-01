03cdd76a6a2daeea60b5d6977f70fc98dd6ff0b6
Mishabuzov <mikhaijl7@gmail.com>
2020-09-01 13:34:31 +0300

    activity callback initialization replaced with setter to prevent memory leak when device is rotated.

--- app/src/main/java/com/example/weatherapp/screen/choose_city/ChooseCityActivity.kt
+++ app/src/main/java/com/example/weatherapp/screen/choose_city/ChooseCityActivity.kt
@@ -2,7 +2,6 @@ package com.example.weatherapp.screen.choose_city
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp.R
@@ -14,8 +13,7 @@ import kotlinx.android.synthetic.main.activity_choose_city.*
 class ChooseCityActivity : AppCompatActivity(), ChooseCityViewModel.RefreshDataCallback {
 
     private val chooseCityViewModel by lazy {
-        ViewModelProvider(this, ChooseCityViewModel.ChooseCityViewModelFactory(this))
-            .get(ChooseCityViewModel(this)::class.java)
+        ViewModelProvider(this).get(ChooseCityViewModel::class.java)
     }
 
     private val adapter = ChooseCityAdapter { CityActivity.start(this, it.first, it.second) }
@@ -24,10 +22,11 @@ class ChooseCityActivity : AppCompatActivity(), ChooseCityViewModel.RefreshDataC
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_city)
 
+        chooseCityViewModel.refreshDataCallback = this
         initAdapter()
 
         val currentLiveData = chooseCityViewModel.getCityWeather()
-        currentLiveData.observe(this, Observer {
+        currentLiveData.observe(this, {
             it?.let { refreshAdapter(it) }
         })
         if (currentLiveData.value.isNullOrEmpty()) {
--- app/src/main/java/com/example/weatherapp/screen/choose_city/ChooseCityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/screen/choose_city/ChooseCityViewModel.kt
@@ -3,18 +3,20 @@ package com.example.weatherapp.screen.choose_city
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.Config
-import com.example.weatherapp.content.CurrentWeather
 import com.example.weatherapp.WeatherProvider
+import com.example.weatherapp.content.CurrentWeather
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class ChooseCityViewModel(private val refreshDataCallback: RefreshDataCallback) : ViewModel() {
+class ChooseCityViewModel() : ViewModel() {
 
     private val currentWeatherList: MutableLiveData<List<CurrentWeather>> = MutableLiveData()
 
     fun getCityWeather(): MutableLiveData<List<CurrentWeather>> = currentWeatherList
+
+    lateinit var refreshDataCallback: RefreshDataCallback
+
     fun processCurrentWeatherRequest() =
         WeatherProvider.weatherRepository
             .getCurrentWeather(Config.citiesIds.values.joinToString(separator = ","))
@@ -32,15 +34,6 @@ class ChooseCityViewModel(private val refreshDataCallback: RefreshDataCallback) 
                     )
                 }
             )
-
-
-    class ChooseCityViewModelFactory(private val refreshDataCallback: RefreshDataCallback) :
-        ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return modelClass.getConstructor(RefreshDataCallback::class.java)
-                .newInstance(refreshDataCallback)
-        }
-    }
 
     interface RefreshDataCallback {
         fun refreshAdapter(currentWeathers: List<CurrentWeather>)
